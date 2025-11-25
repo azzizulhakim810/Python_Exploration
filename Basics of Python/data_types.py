74f168edbb082ee29cda1e1675be60fab9743099
azzizulhakim810 <azzizul.hakim202@gmail.com>
2025-11-25 18:36:06 +0600

    Adding more data types

---
+++ Basics of Python/data_types.py
@@ -0,0 +1,14 @@
+import sys
+print(sys.version)
+
+# Integer 
+num1 = 5
+print(num1, 'is of type', type(num1))
+
+# Float 
+num2 = 2.0
+print(num2, 'is of type', type(num2))
+
+# Complex 
+num3 = 1+2j
+print(num3, 'is of type', type(num3))
