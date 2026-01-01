67a0e6a65c98aa8b784e7316018301e68e9561fc
azzizulhakim810 <azzizul.hakim202@gmail.com>
2026-01-01 12:52:03 +0600

    Exploring Functions

---
+++ Basics of Python/_functions/complex.py
@@ -0,0 +1,16 @@
+def fetch_sales():
+  print(f"Fetching the sales data")
+
+def filter_valid_sales():
+  print(f"Filtering valid sales data")
+
+def summarize_data():
+  print(f"Summarizing sales data")
+
+def generate_report():
+  fetch_sales()
+  filter_valid_sales()
+  summarize_data()
+  print(f"Report is ready")
+
+generate_report()
---
+++ Basics of Python/_functions/duplications.py
@@ -0,0 +1,6 @@
+def print_order(name, chai_type):
+  print(f"{name} ordered {chai_type} chai!")
+
+print_order("Saiful Bhai", "Milk")
+print_order("Semmon", "Coffee")
+print_order("Sagor Bhai", "Lemon")
