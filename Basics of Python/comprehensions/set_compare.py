2f25aa70347f7150a91a10892797d6dc5ba8dd7e
azzizulhakim810 <azzizul.hakim202@gmail.com>
2026-01-02 23:53:01 +0600

    Explored set comprehension

---
+++ Basics of Python/comprehensions/set_compare.py
@@ -0,0 +1,27 @@
+# Simple set comprehension ----------------------------------
+
+favorite_chais = [
+  "Masala Chai",
+  "Green Tea",
+  "Masala Chai",
+  "Lemon Tea",
+  "Green Tea",
+  "Elaichi Chai"
+]
+
+unique_chai = {chai for chai in favorite_chais}
+# print (unique_chai)
+
+
+# Complex Example (Extract spices from dictionary using set comprehension ------------------
+
+recipes = {
+  "Masala Chai": ["ginger", "cardamom", "clove"],
+  "Elaichi Chai": [ "cardamom", "milk"],
+  "Spicy Chai": ["ginger", "black pepper", "clove"]
+}
+
+# Set Comprehension =  {expression for item in iterable if condition (we can use other things like loops instead of condition)} 
+unique_spices = {spice for ingredients in recipes.values() for spice in ingredients} # we should use the ultimate value(spice) in the first place not "ingredients"
+
+print(unique_spices)
