fceaa408a22a81b6ea9c6e974b48f868bb800263
azzizulhakim810 <azzizul.hakim202@gmail.com>
2026-01-02 19:20:53 +0600

    Exploring Comprehension - Explored list comprehension already

---
+++ Basics of Python/comprehensions/list_compre.py
@@ -0,0 +1,13 @@
+menu = [
+  "Masala Chai",
+  "Iced Lemon Tea",
+  "Green Tea",
+  "Iced Peach Tea",
+  "Ginger Tea"
+]
+
+# List Comprehension =  [expression for item in iterable if condition]
+order_tea = [tea for tea in menu if "Iced" in tea]
+measured_tea = [my_tea for my_tea in menu if len(my_tea) < 12]
+print("Order: ", order_tea)
+print("Measured: ", measured_tea)
