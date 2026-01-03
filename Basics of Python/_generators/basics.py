04d2f9d61cd772aa37fd1d629ec7efc04c4112de
azzizulhakim810 <azzizul.hakim202@gmail.com>
2026-01-03 22:51:27 +0600

    Exploring Generators - Basic Covered

---
+++ Basics of Python/_generators/basics.py
@@ -0,0 +1,31 @@
+# Generators - speacial keyword "yield"
+
+# You save memory
+# You don't want the results immediately
+# Lazy evaluation 
+
+def serve_chai():
+  yield "Cup 1: Masala Chai"
+  yield "Cup 2: Ginger Chai"
+  yield "Cup 3: Elaichi Chai"
+
+stall = serve_chai() # yield pauses the function to call, it's just keep the reference here
+for cup in stall: # it's the main calling & printing
+  print(cup)
+
+
+# Another one -----------------------------------------------
+
+def get_chai_list():
+  return ["Cup 1", "Cup 2", "Cup 3"]
+
+def get_chai_gen():
+  yield "Cup 1"
+  yield "Cup 2"
+  yield "Cup 3"
+
+chai = get_chai_gen() # next pauses the stream & take a break & execute
+print(next(chai)) # It's just printing the first one by using next
+print(next(chai)) # It's just printing the second one by using next 
+print(next(chai)) # It's just printing the third one by using next
+print(next(chai)) # It's showing error
