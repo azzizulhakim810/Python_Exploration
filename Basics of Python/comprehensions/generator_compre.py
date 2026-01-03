5b70920207567aef6f9971a14eec894ecaca3159
azzizulhakim810 <azzizul.hakim202@gmail.com>
2026-01-03 22:37:01 +0600

    Generator comprehension Updated

--- Basics of Python/comprehensions/generator_compre.py
+++ Basics of Python/comprehensions/generator_compre.py
@@ -1,6 +1,9 @@
 daily_sales = [3, 10, 8, 5, 9, 30, 15]
 
 
-# Dictionary Comprehension =  (expression for item in iterable if condition (we can use other things like loops instead of condition)) - It's working fast as it's streaming & working. That's why we're putting sum in front of the expression
+# Generator Comprehension =  (expression for item in iterable if condition (we can use other things like loops instead of condition)) - It's working fast as it's streaming & working. That's why we're putting sum in front of the expression
 total_cups = sum(sale for sale in daily_sales if sale > 5)
 print(total_cups)
+
+# [x for x in items] - make entire list in memory 
+# (x for x in items) - like a stream
