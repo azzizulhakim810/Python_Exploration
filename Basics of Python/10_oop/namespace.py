d1d24c24ad9aed28402ff1741b7659b951cf2796
azzizulhakim810 <azzizul.hakim202@gmail.com>
2026-01-08 00:07:54 +0600

    Exploring OOP - Namespace updated

--- Basics of Python/10_oop/namespace.py
+++ Basics of Python/10_oop/namespace.py
@@ -1,15 +1,23 @@
 class Chai:
-  origin = "Bangladesh"
+  origin = "Bangladesh" # Declearing this called as properties
 
 print(Chai.origin)
 
-Chai.is_hot = True
+Chai.is_hot = True # Assign another property from outside
 print("Chai Hot Status: ", Chai.is_hot)
 
-masala = Chai()
+
+# Creating objects from class Chai --------------------------------
+
+masala = Chai() 
 print("Masala Original Country: ", masala.origin)
 print("Masala Chai Hot Status: ", masala.is_hot)
 
 masala.is_hot = False
 print("Masala Chai Hot Status: ", masala.is_hot)
 print("Chai Hot Status: ", Chai.is_hot)
+
+# Putting another property into the object doesn't mean, it'll be available for class itself 
+masala.flavor = "Spicy"
+print(f"Masala Flavor: {masala.flavor}")
+print(f"Chai Flavor: {Chai.flavor}") # doesn't exist
