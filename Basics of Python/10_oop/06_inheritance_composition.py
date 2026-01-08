a6791cba6d0f71bc85561d5276e27dc66d4e663b
azzizulhakim810 <azzizul.hakim202@gmail.com>
2026-01-08 13:10:34 +0600

    Exploring OOP - Inheritance & Composition

---
+++ Basics of Python/10_oop/06_inheritance_composition.py
@@ -0,0 +1,35 @@
+class BaseChai:
+  def __init__(self, type_):
+    self.type = type_
+
+  # Method 
+  def prepare(self):
+    print(f"Preparing {self.type} chai....")
+
+# Inheritance
+class MasalaChai(BaseChai): # Inherite from parent
+  
+  # Method 
+  def add_spices(self):
+    print(f"Adding Cardamom, Ginger, Cloves")
+
+
+# Composition
+class ChaiShop:
+   chai_cls = BaseChai # an attribute which holds a reference of BaseChai
+
+   def __init__(self):
+     self.chai = self.chai_cls("Regular") # self.chai holds self.chai_cls reference & pass "Regular" as BaseChai type
+    
+   def serve(self):
+    print(f"Serving {self.chai.type} chai in the shop")
+    self.chai.prepare()
+
+class FancyChaiShop(ChaiShop):
+  chai_cls = MasalaChai # an attribute which holds a reference of MasalaChai
+
+shop = ChaiShop()
+fancy = FancyChaiShop()
+shop.serve()
+fancy.chai.prepare()
+fancy.chai.add_spices()
