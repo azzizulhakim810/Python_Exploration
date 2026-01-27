a20f7ce77546a0221f673ee0e76d45cfb537366c
azzizulhakim810 <azzizul.hakim202@gmail.com>
2026-01-28 01:38:44 +0600

    Disconvering Exceptions - a order history to explain everything

---
+++ Basics of Python/11_exceptions/07_complete_order.py
@@ -0,0 +1,21 @@
+class InvalidChaiError(Exception): pass
+
+def bill(flavor, cups):
+  menu = {"masala": 20, "ginger": 40}
+  
+  try:
+    if flavor not in menu:
+      raise InvalidChaiError("That is not available")
+    if not isinstance(cups, int):
+      raise TypeError("Number of cups must be an integer")
+    total = menu[flavor] * cups
+    print(f"Your bill for {cups} cups of {flavor} chai: {total}")
+  except Exception as e:
+    print("Error: ", e)
+  finally: 
+    print("Thanks for coming")
+
+
+bill("mint", 2)
+bill("masala", "two")
+bill("masala", 5)
